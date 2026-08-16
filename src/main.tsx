0eee9a183f9275e9ca6ebaaf922fd10aee830572
cutehamster290-tech <cutehamster290@gmail.com>
2026-08-16 09:21:29 +0200

    game modified.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { GameContainer } from './GameContainer';
-import './index.css';
+import {StrictMode, useEffect, useRef} from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+
+    ctx.fillStyle = '#111'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(100, 100, 40, 100)
+  })
+
+  return (
+    <canvas ref={canvasRef}></canvas>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GameContainer />
-  </React.StrictMode>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
